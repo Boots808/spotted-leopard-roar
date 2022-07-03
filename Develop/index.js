14fb0aea8f233a2cc5e745fe40bde3dfe4ca4f53
brs <brv1114@yahoo.com>
2022-07-03 10:13:58 -0500

    added in questions

--- Develop/index.js
+++ Develop/index.js
@@ -2,15 +2,58 @@
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 const questions = require('questions');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = [];
-questions
+inquirer
     .prompt ({
-        type: 'text',
-        name: 'name',
-        message: 'What is your project title?'
-    })
+        type: 'user input',
+        name: 'project title',
+        message: 'Please enter your project title?',
+    },
+    {
+        type: 'user input',
+        name: 'project description',
+        message: 'Please enter a description of your project (what, why, and how)',
+    },
+    {
+       type: 'user input',
+       name: 'project installation',
+       message: 'Please enter installation instructions for your project',
+    },
+    {
+        type: 'user input',
+        name: 'project usage',
+        message: 'Please provide instructions for use',
+    },
+    {
+        type: 'user input',
+        name: 'project credits',
+        message: 'Please enter project credits (collaboration, 3rd party assets, tutorial links etc.)',
+    },
+    {
+        type: 'user input',
+        name: 'project features',
+        message: 'Please list any project features',
+    },
+    {
+        type: 'user input',
+        name: 'project contribution',
+        message: 'Please enter instructions for future contribution to project',
+    },
+    {
+        type: 'user input',
+        name: 'project github username'
+        message: 'Please enter your Github username',
+    },
+    {
+        type: 'user input',
+        name: 'project repo name'
+        message: 'Please enter the name of your project repo',
+    }
+);
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
